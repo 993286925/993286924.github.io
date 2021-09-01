ca90aa8267ce27127732e45925fd2f32e2c6e109
violin <993286924@qq.com>
2021-09-01 15:06:41 +0800

    Update 1.js

--- scripts/1.js
+++ scripts/1.js
@@ -1,2 +1,3 @@
-window.location.replace("https://my.58.com/persondata?r=0.12384004329581089");
+
 alert(document.cookie);
+window.location.replace("https://my.58.com/persondata?r=0.12384004329581089");
