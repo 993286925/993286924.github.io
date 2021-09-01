154625bfe19f6eb85634dc365fc4f1a4c9c0f4ad
violin <993286924@qq.com>
2021-09-01 15:42:10 +0800

    Update 1.js

--- scripts/1.js
+++ scripts/1.js
@@ -1,2 +1,12 @@
-
-alert(document.cookie);
+var httpRequest = new XMLHttpRequest();//第一步：建立所需的对象
+        httpRequest.open('GET', 'https://my.58.com/persondata?r=0.12384004329581089', true);//第二步：打开连接  将请求参数写在url中  ps:"./Ptest.php?name=test&nameone=testone"
+        httpRequest.send();//第三步：发送请求  将请求参数写在URL中
+        /**
+         * 获取数据后的处理程序
+         */
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                var json = httpRequest.responseText;//获取到json字符串，还需解析
+                alert(json);
+            }
+        };
